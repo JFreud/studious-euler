6a17c9fc08ed23afe9636a9189b57930a76aa1c6
JFreud <jrm.frdnbrg@gmail.com>
2017-09-16 17:48:17 -0400

    finished sum of primes

--- euler.c
+++ euler.c
@@ -1,14 +1,16 @@
-#include stdio.h
-#include stlib.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 
-int sum_primes(int limit) {
-  int sum = 0;
+long sum_primes(int limit) {
+  long sum = 0;
   int i;
-  for(i = 1; i < limit; i++) {
+  int toAdd;
+  for(i = 2; i < limit; i++) {
     int j;
-    int toAdd = 0;
-    for(j = floor(sqrt(i)); j > 1; i++) {
+    toAdd = 0;
+    for(j = floor(sqrt(i)); j > 1; j--) {
       if (i % j == 0){
 	toAdd = 1;
       }
@@ -23,5 +25,7 @@ int sum_primes(int limit) {
 
 
 int main() {
-  printf(sum_primes(30));
+  long thesum = sum_primes(2000000);
+  //printf("%d\n", 5);
+  printf("%ldd\n", thesum);
 }
