f3f1edd27975ed448e8d73e1d49c345782dd1840
JFreud <jrm.frdnbrg@gmail.com>
2017-09-16 18:02:25 -0400

    added smallest multiple solution

--- euler.c
+++ euler.c
@@ -22,10 +22,27 @@ long sum_primes(int limit) {
   return sum;
 }
 
-
+int smol_multi(int cap) {
+  int smol = 1;
+  int i;
+  int isDivis;
+  while (isDivis != 1) {
+    isDivis = 1;
+    for (i = 1; i < cap; i++) {
+      if (smol % i != 0) {
+	isDivis = 0;
+      }
+    }
+    smol++;
+  }
+  return smol - 1;
+}
 
 int main() {
   long thesum = sum_primes(2000000);
   //printf("%d\n", 5);
-  printf("%ld\n", thesum);
+  printf("The sum of primes below 2000000 is %ld\n", thesum);
+
+  int multiple = smol_multi(20);
+  printf("The smallest positive number divisible by nums 1-20 is %d\n", multiple);
 }
